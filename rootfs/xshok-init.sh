d014ed1709d013ec18e94380226e9e7f053588d6
eXtremeSHOK <admin@extremeshok.com>
2019-04-01 06:22:30 +0200

    notice

--- rootfs/xshok-init.sh
+++ rootfs/xshok-init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+################################################################################
+# This is property of eXtremeSHOK.com
+# You are free to use, modify and distribute, however you may not remove this notice.
+# Copyright (c) Adrian Jon Kriel :: admin@extremeshok.com
+################################################################################
 ## enable case insensitve matching
 shopt -s nocaseglob
 
