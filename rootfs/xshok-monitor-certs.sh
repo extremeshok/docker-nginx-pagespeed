698b8ff886fcd269d8c8b9ee289e8cedb089db52
eXtremeSHOK <admin@extremeshok.com>
2019-12-16 23:22:22 +0200

    recursive monitor

--- rootfs/xshok-monitor-certs.sh
+++ rootfs/xshok-monitor-certs.sh
@@ -4,11 +4,12 @@
 # You are free to use, modify and distribute, however you may not remove this notice.
 # Copyright (c) Adrian Jon Kriel :: admin@extremeshok.com
 ################################################################################
-# Monitor the /certs directory and reload nginx after 30s on changes
+# Monitor the /certs directory and sub directories
+# When a chnage is detected, wait 30 seconds and issue the nginx reload command
 ################################################################################
 while true
 do
-        /usr/bin/inotifywait -e create -e modify -e delete /certs/
+        /usr/bin/inotifywait -e create -e modify -e delete -r /certs/
         echo "Change detetect in /certs"
         sleep 30s
         echo "Reloading nginx"
